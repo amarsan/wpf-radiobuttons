d31d8cd49259d77785b6398a5871ae9a57aab290
Anne Marsan <anne.marsan@atomicobject.com>
2013-11-17 10:13:57 -0500

    Started the basic project. I have 3 different radio button groups so far.

---
+++ RadioButtonDemo/RadioButtonDemo/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioButtonDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            VegetableDishes = new Collection<string> { "Butternut Squash Soufle", "Roasted Celery Root", "Sauteed Brussel Sprouts", "Glazed Carrots"};
+            SelectedVegetableDish = VegetableDishes.Last();
+        }
+
+        private ICollection<string> vegetableDishes; 
+        public ICollection<string> VegetableDishes 
+        {
+            get { return vegetableDishes; }
+            set 
+            { 
+                vegetableDishes = value;
+                onNotifyPropertyChanged("VegetableDishes");
+            }
+        }
+
+        private string selectedVegetableDish;
+        public string SelectedVegetableDish
+        {
+            get { return selectedVegetableDish; }
+            set 
+            { 
+                selectedVegetableDish = value;
+                onNotifyPropertyChanged("SelectedVegetableDish");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void onNotifyPropertyChanged(string propertyName)
+        {
+            var eventToFire = PropertyChanged;
+            if (eventToFire != null)
+                eventToFire(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
